d245648c40c096fbf4f1e62c18acfd676381d68b
Chad Cummings <5791160+c4cummings@users.noreply.github.com>
2025-08-25 15:52:51 -0600

    feat: Generate colors, shapes, and fonts, and theme using Material Theme Builder.

---
+++ app/src/main/java/com/cricut/androidassessment/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package com.cricut.androidassessment.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(topEnd = 16.dp, bottomEnd = 16.dp, bottomStart = 16.dp),
+    small = RoundedCornerShape(topEnd = 16.dp, bottomEnd = 16.dp, bottomStart = 16.dp),
+    medium = RoundedCornerShape(topEnd = 16.dp, bottomEnd = 16.dp, bottomStart = 16.dp),
+    large = RoundedCornerShape(topEnd = 16.dp, bottomEnd = 16.dp, bottomStart = 16.dp),
+    extraLarge = RoundedCornerShape(topEnd = 16.dp, bottomEnd = 16.dp, bottomStart = 16.dp)
+)
--- app/src/main/java/com/cricut/androidassessment/ui/theme/Type.kt
+++ app/src/main/java/com/cricut/androidassessment/ui/theme/Type.kt
@@ -1,34 +1,71 @@
 package com.cricut.androidassessment.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import com.cricut.androidassessment.R
 
-// Set of Material typography styles to start with
+// Generated from Material Theme Builder: https://material-foundation.github.io/material-theme-builder/
+
+private val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+private val displayFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Simonetta"),
+        fontProvider = provider,
+    )
+)
+
+private val headlineFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Simonetta"),
+        fontProvider = provider,
+    )
+)
+
+private val titleFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Simonetta"),
+        fontProvider = provider,
+    )
+)
+
+private val bodyFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Simonetta"),
+        fontProvider = provider,
+    )
+)
+
+private val labelFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Simonetta"),
+        fontProvider = provider,
+    )
+)
+
+// Default Material 3 typography values
+private val baseline = Typography()
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = headlineFontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = headlineFontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = headlineFontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = titleFontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = titleFontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = titleFontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = labelFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = labelFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = labelFontFamily),
 )
