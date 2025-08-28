429caf4182a400ce75e1b8f55dc27419a93ea5ea
Chad Cummings <5791160+c4cummings@users.noreply.github.com>
2025-08-27 22:57:27 -0600

    feat: Use the new UI state object to build a basic AssessmentViewModel.

--- app/src/main/java/com/cricut/androidassessment/ui/AssessmentViewModel.kt
+++ app/src/main/java/com/cricut/androidassessment/ui/AssessmentViewModel.kt
@@ -1,6 +1,11 @@
 package com.cricut.androidassessment.ui
 
 import androidx.lifecycle.ViewModel
+import com.cricut.androidassessment.data.BooleanQuestion
+import com.cricut.androidassessment.data.MultipleSelectionQuestion
+import com.cricut.androidassessment.data.Question
+import com.cricut.androidassessment.data.ShortAnswerQuestion
+import com.cricut.androidassessment.data.SingleSelectionQuestion
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -10,9 +15,50 @@ import javax.inject.Inject
 @HiltViewModel
 class AssessmentViewModel
 @Inject constructor() : ViewModel() {
-    // TODO implement state variables and functions
 
-    // Remove/Modify this
-    private val _uiState = MutableStateFlow<String>("Hello World!")
-    val uiState: StateFlow<String> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow(AssessmentUiState())
+    val uiState: StateFlow<AssessmentUiState> = _uiState.asStateFlow()
+
+    private lateinit var _questions: List<Question<*, *>>
+
+    init {
+        loadQuestions()
+    }
+
+    fun loadQuestions() {
+        _questions = listOf(
+            BooleanQuestion(
+                statement = "Is this a question?",
+                answer = true,
+                affirmative = "Yes",
+                negative = "No"
+            ),
+            MultipleSelectionQuestion(
+                statement = "Which of these?",
+                options = setOf("One", "Two", "Three"),
+                answer = setOf("One", "Three")
+            ),
+            ShortAnswerQuestion(
+                statement = "What is the meaning of life?",
+                answer = "42"
+            ),
+            SingleSelectionQuestion(
+                statement = "Which one of these?",
+                options = setOf("One", "Two", "Three"),
+                answer = "Two"
+            )
+        )
+
+        _uiState.value = AssessmentUiState(
+            currentQuestion = _questions[0],
+            questionCount = _questions.size,
+            currentPosition = 0
+        )
+    }
+
+    fun <T> checkAnswer(answer: T) {
+
+    }
+
+
 }
